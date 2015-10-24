9cca33a8ddfbc835ee124a23ad9223c5d4f23d90
sgoutouly <sgoutouly@gmail.com>
2015-10-24 18:46:11 +0200

    Using before and after, adding one assert

--- src/test/java/com/sylvaingoutouly/N1QL.java
+++ src/test/java/com/sylvaingoutouly/N1QL.java
@@ -3,7 +3,12 @@ package com.sylvaingoutouly;
 import static com.couchbase.client.java.query.N1qlQuery.simple;
 import static com.couchbase.client.java.query.Select.select;
 import static com.couchbase.client.java.query.dsl.Expression.i;
+import static java.lang.System.out;
+import static org.junit.Assert.assertEquals;
+import junit.framework.Assert;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.couchbase.client.java.Bucket;
@@ -16,18 +21,33 @@ import com.couchbase.client.java.query.N1qlQueryResult;
 
 public class N1QL {
 
-	@Test public void n1qlSelectOnBeer() {
-		CouchbaseEnvironment env = DefaultCouchbaseEnvironment.create();
-		CouchbaseCluster cluster = CouchbaseCluster.create(env);
-		Bucket beerSample = cluster.openBucket("travel-sample");
+	private Bucket beerSample;
+	private CouchbaseCluster cluster;
+	
+	
+	@Test public void selectOnBeerSync() {
 		
+		// Needed to use N1QL
 		beerSample.query(simple(Index.createPrimaryIndex().on("beer-sample")));
 		
-		N1qlQueryResult beers = beerSample.query(simple(select("*").from(i("beer-sample")).limit(10)));		
-		System.out.println(beers.errors().toString());
-		System.out.println(beers.info().elapsedTime());
-		System.out.println(beers.info().resultCount());
+		// Simple select on sample beer bucket
+		N1qlQueryResult beers = beerSample.query(simple(select("*").from(i("beer-sample")).limit(10)));
 		
+		assertEquals(10, beers.info().resultCount());
+		
+		out.println(beers.errors().toString());
+		out.println(beers.info().elapsedTime());
+		out.println(beers.info().resultCount());
+	}
+	
+	
+	@Before public void before() {
+		CouchbaseEnvironment env = DefaultCouchbaseEnvironment.create();
+		cluster = CouchbaseCluster.create(env);
+		beerSample = cluster.openBucket("beer-sample");
+	}
+	
+	@After public void after() {
 		cluster.disconnect();
 	}
 
